8e722c3512a657321eea9ee8d02c66ea72730c22
mogakamo <mogaka.amo254@gmail.com>
2023-04-25 18:56:49 +0300

    [schema]: updated the database with the user schema

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MailService } from './mail.service';
+import {MailerModule} from '@nestjs-modules/mailer'
 
 @Module({
   providers: [MailService]
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {SwaggerModule, DocumentBuilder} from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Art Galore')
+    .setDescription('This is the base api for art galore')
+    .setVersion('0.0.1')
+    .addTag('art')
+    .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document)
+
   await app.listen(3000);
 }
 bootstrap();
