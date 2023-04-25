93584e564f7ccb7be1f9984048c994489a52dc4c
mogakamo <mogaka.amo254@gmail.com>
2023-04-25 19:02:22 +0300

    [chore]: configuring prisma for global use

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
-
+import { INestApplication, Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 @Injectable()
-export class PrismaService {}
+export class PrismaService extends PrismaClient {
+  async enableShutdownHooks(app: INestApplication) {
+    this.$on('beforeExit', async () => await app.close());
+  }
+}
